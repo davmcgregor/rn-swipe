a6a51c654702b8a2fc2be277198a43ae61044bee
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-19 19:05:05 +0000

    Add Deck boilerplate

--- src/Ball.js
+++ src/Ball.js
@@ -1,17 +1,30 @@
-import React from 'react'
-import {View, StyleSheet} from 'react-native'
+import React, { useEffect, useState } from 'react';
+import { View, Animated, StyleSheet, Text } from 'react-native';
 
 const Ball = () => {
-  return (<View style={styles.ball}w></View>)
-}
+  const [position, setPosition] = useState(new Animated.ValueXY(0, 0));
+
+  useEffect(() => {
+    Animated.spring(position, {
+      toValue: { x: 200, y: 500 },
+    }).start();
+  }, []);
+
+  return (
+    <Animated.View style={position.getLayout()}>
+      <View style={styles.ball}></View>
+      <Text>Hi</Text>
+    </Animated.View>
+  );
+};
 const styles = StyleSheet.create({
   ball: {
     height: 60,
     width: 60,
     borderRadius: 30,
     borderWidth: 30,
-    borderColor: 'black'
-  }
+    borderColor: 'black',
+  },
 });
 
-export default Ball
+export default Ball;
---
+++ src/Deck.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Animated, StyleSheet } from 'react-native';
+
+const Deck = () => {
+  return <View />;
+};
+
+const styles = StyleSheet.create({});
+
+export default Deck;
