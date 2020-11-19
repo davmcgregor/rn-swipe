18b9d9e15817e6f61f01e6f9191e51e2a21ef18b
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-19 21:16:42 +0000

    Ad pan responder event handlers

--- src/Deck.js
+++ src/Deck.js
@@ -2,8 +2,15 @@ import React, { useRef } from 'react';
 import { View, Animated, StyleSheet, PanResponder } from 'react-native';
 
 const Deck = ({ data, renderCard }) => {
-  
-  const panResponder = useRef(PanResponder.create({})).current;
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (evt, gestureState) => {
+        console.log(gesture);
+      },
+      onPanResponderRelease: () => {},
+    })
+  ).current;
 
   const renderCards = () => {
     return data.map((item) => renderCard(item));
