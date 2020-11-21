ad0361decab47d5cc3186abae6d594bb6db1d4fa
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-21 16:23:15 +0000

    Add spring back to default

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,13 @@
 import React, { useRef } from 'react';
-import { View, Animated, StyleSheet, PanResponder } from 'react-native';
+import {
+  View,
+  Animated,
+  StyleSheet,
+  PanResponder,
+  Dimensions,
+} from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 const Deck = ({ data, renderCard }) => {
   const pan = useRef(new Animated.ValueXY()).current;
@@ -15,18 +23,27 @@ const Deck = ({ data, renderCard }) => {
       },
       onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }]),
       onPanResponderRelease: () => {
-        pan.flattenOffset();
+        Animated.spring(
+          pan, // Auto-multiplexed
+          { toValue: { x: 0, y: 0 }, friction: 6 } // Back to zero
+        ).start();
       },
     })
   ).current;
 
   const getCardStyle = () => {
     const rotate = pan.x.interpolate({
-      inputRange: [-500, 0, 500],
+      inputRange: [-SCREEN_WIDTH * 2, 0, SCREEN_WIDTH * 2],
       outputRange: ['-120deg', '0deg', '120deg'],
     });
 
+    // const color = pan.x.interpolate({
+    //   inputRange: [-500, 500],
+    //   outputRange: ['rgb(0,0,0)', 'rgb(51,250,170)'],
+    // });
+
     return {
+      // backgroundColor: color,
       transform: [{ translateX: pan.x }, { translateY: pan.y }, { rotate }],
     };
   };
