6146b2d75781832446156e724b1a5fc4964472ec
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-21 14:57:59 +0000

    Connect panResponder to Deck

--- src/Deck.js
+++ src/Deck.js
@@ -2,13 +2,21 @@ import React, { useRef } from 'react';
 import { View, Animated, StyleSheet, PanResponder } from 'react-native';
 
 const Deck = ({ data, renderCard }) => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
   const panResponder = useRef(
     PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (evt, gestureState) => {
-        console.log(gesture);
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        pan.setOffset({
+          x: pan.x._value,
+          y: pan.y._value,
+        });
       },
-      onPanResponderRelease: () => {},
+      onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }]),
+      onPanResponderRelease: () => {
+        pan.flattenOffset();
+      },
     })
   ).current;
 
@@ -16,7 +24,18 @@ const Deck = ({ data, renderCard }) => {
     return data.map((item) => renderCard(item));
   };
 
-  return <View>{renderCards()}</View>;
+  return (
+    <View>
+      <Animated.View
+        style={{
+          transform: [{ translateX: pan.x }, { translateY: pan.y }],
+        }}
+        {...panResponder.panHandlers}
+      >
+        {renderCards()}
+      </Animated.View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
